9952786f6f52acc5e190cc8c6f0b532bc4d5b7fc
Enas <enas.elmershati@gmail.com>
2023-03-05 00:44:38 +0100

    Files

---
+++ headless-strapi/config/middlewares.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': ["'self'", 'data:', 'blob:', `${process.env.SUPABASE_API_URL}`],
+        }
+      }
+    }
+  },
+
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
